b6624f51f5ec1db07728a210697ddffca86d7ec5
999 <999@hyeon.pro>
2024-04-04 17:41:11 +0000

    fix: update clean script

--- scripts/php_env.sh
+++ scripts/php_env.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-DEFAULT_VERSION="v1"
-
-read -p "Enter the version (default: $DEFAULT_VERSION, e.g., v1, v2, v3): " input
-
-VERSION="$DEFAULT_VERSION"
-if [ ! -z "$input" ]; then
-    VERSION="$input"
-fi
-
-echo "php_value upload_max_filesize 512M" >> volume/"$VERSION"/wp/.htaccess
-echo "php_value post_max_size 512M" >> volume/"$VERSION"/wp/.htaccess
-echo "php_value memory_limit 256M" >> volume/"$VERSION"/wp/.htaccess
-echo "php_value max_execution_time 300" >> volume/"$VERSION"/wp/.htaccess
-echo "php_value max_input_time 300" >> volume/"$VERSION"/wp/.htaccess
+echo "php_value upload_max_filesize 512M" >> volume/wp/.htaccess
+echo "php_value post_max_size 512M" >> volume/wp/.htaccess
+echo "php_value memory_limit 256M" >> volume/wp/.htaccess
+echo "php_value max_execution_time 300" >> volume/wp/.htaccess
+echo "php_value max_input_time 300" >> volume/wp/.htaccess
