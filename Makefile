84b0dc33fadc0e9efdb4775dba4729c8c0f09cb5
999 <999@hyeon.pro>
2024-04-04 16:40:08 +0000

    feat: 환경변수 생성까지 모두 완료

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ init:
 	chmod +x ./scripts/init.sh && sh ./scripts/init.sh
 
 clean:
-	rm -r apps volume
+	rm -r apps volume .env
 
 p:
 	sudo chown -R 33:33 apps/theme volume/ && sudo chmod -R 775 apps/theme volume/
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,28 +2,35 @@
 
 DEFAULT_PROJECT_NAME="wordpress"
 
-read -p "Choose an environment (d=development, p=production): " choice
-read -p "Enter project name (default: $DEFAULT_PROJECT_NAME): " input
+read -p "프로젝트 환경을 선택하세요 (d=development, p=production): " choice
+read -p "프로젝트 이름을 입력하세요 (default: $DEFAULT_PROJECT_NAME): " input
+read -p "프로젝트 도메인을 입력하세요: " DOMAIN
 
 # 프로젝트 이름이 입력되지 않은 경우 기본값 사용
 if [ -z "$input" ]; then
     input=$DEFAULT_PROJECT_NAME
 fi
 
+echo "현재프로젝트명: ${input}"
+
+echo "프로젝트 셋팅 시작 🛫"
+
 # 폴더생성
 mkdir -p volume/db
 mkdir -p volume/wp
 
+echo "1. 볼륨 폴더 생성"
+
 # 환경 설정에 따른 조건 분기
 if [ "$choice" = "p" ]; then
     # 프로덕션 환경 설정
-    echo "현재 환경 - 프로덕션"
     if [ -f docker-compose.prod.yaml ]; then
         mv docker-compose.prod.yaml docker-compose.yaml
     else
         echo "docker-compose.prod.yaml 파일이 없습니다."
         exit 1
     fi
+    echo "2. 프로덕션 환경설정 완료"
 elif [ "$choice" = "d" ]; then
     # 개발 환경 설정
     mkdir -p apps/"$input"
@@ -37,24 +44,25 @@ elif [ "$choice" = "d" ]; then
         echo "docker-compose.yaml 파일이 없습니다."
         exit 1
     fi
+    echo "2. 개발 환경설정 완료"
 else
-    echo "잘못된 선택입니다. 'd' 또는 'p'를 입력해주세요."
+    echo "잘못된 선택입니다. 'd' 또는 'p'를 입력해주세요. ⛔️"
     exit 1
 fi
 
-echo "환경변수 설정"
+echo "3. 환경변수 설정"
 
 # WORDPRESS_PASSWORD 생성 및 설정
 WORDPRESS_PASSWORD=$(openssl rand -base64 48)
-echo "WORDPRESS_PASSWORD=${WORDPRESS_PASSWORD}\n" >> .env
+echo "WORDPRESS_PASSWORD=${WORDPRESS_PASSWORD}" >> .env
 
 # 프로젝트 이름 설정
-PROJECT_NAME=$input
-echo "PROJECT_NAME=${PROJECT_NAME}\n" >> .env
+echo "PROJECT_NAME=${input}" >> .env
 
 # .env 파일에 SERVICE 항목 추가
-echo "SERVICE=$\{PROJECT_NAME\}\n" >> .env
+echo "SERVICE=\${PROJECT_NAME}" >> .env
 
 # 도메인 입력 받기
-read -p "Enter your domain: " DOMAIN
 echo "DOMAIN=${DOMAIN}" >> .env
+
+echo "프로젝트 셋팅 완료 🛬"
